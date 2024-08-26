088e698758270fa2a06cb994f8dbbb7befcd4194
Harshal Chavan <91681985+hcharshal31@users.noreply.github.com>
2024-08-27 03:59:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+for(let i = 1; i <= 20; i++){
+	let output = "";
+	if(i % 3 == 0 && i % 5 == 0){
+		output = "FizzBuzz";
+	} else if(i % 3 == 0){
+		output = "Fizz";
+	} else if(i % 5 == 0){
+		output = "Buzz";
+	} else {
+		output = i;
+	}
+	alert(output);
+}
